562e958a691baa88d91ab7d26bcef23b7e41d4c6
Rongxin Sun <sunrongxin7666@163.com>
2017-08-25 12:18:44 +0800

    TabLayout review

--- app/src/main/java/com/android/srx/github/designsupportlibrarydemo/TabLayoutActivity.java
+++ app/src/main/java/com/android/srx/github/designsupportlibrarydemo/TabLayoutActivity.java
@@ -1,5 +1,7 @@
 package com.android.srx.github.designsupportlibrarydemo;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -20,7 +22,11 @@ public class TabLayoutActivity extends AppCompatActivity {
 
 	private ViewPager mViewPager;
 	private TabLayout mTabLayout;
+	private FloatingActionButton mActionButton;
 	private final int mSize = 8;
+	private List<String> mTitles;
+	private List<String> mWords;
+	private List<Fragment> mFragments;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,50 +34,95 @@ public class TabLayoutActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_tab_layout);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+		initView();
+	}
+
+	private void initView() {
 		mViewPager = (ViewPager) findViewById(R.id.viewpager);
 		mTabLayout = (TabLayout) findViewById(R.id.tabs);
+		mActionButton = (FloatingActionButton) findViewById(R.id.btnFloatingAction);
+		mActionButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				Snackbar.make(v, "Hello SnackBar!", Snackbar.LENGTH_SHORT)
+						.setAction("Undo", new View.OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								// Perform anything for the action selected
+							}
+						})
+						.show();
+			}
+		});
 		initViewPager();
 	}
 
 	private void initViewPager() {
-		final List<String> titles = new StringGenerator("title").generate(mSize);
-		List<String> words = new StringGenerator("Fragment").generate(mSize);
-
-		final List<Fragment> fragments = new ArrayList<>();
-		for (int i = 0; i < mSize; i++) {
-			TabBlankFragment fragment = new TabBlankFragment();
-			fragment.setTitle(words.get(i));
-			fragments.add(fragment);
-		}
+		initData();
 
 		//预加载
-		mViewPager.setOffscreenPageLimit(titles.size()/2);
+		mViewPager.setOffscreenPageLimit(mTitles.size()/2);
 
 		//ViewPager滑动监听
+		mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+			}
+
+			@Override
+			public void onPageSelected(int position) {
+				//Log.e(TAG, "onPageSelected: "+position);
+				if(position==0){
+					mActionButton.setVisibility(View.GONE);
+				} else {
+					mActionButton.setVisibility(View.VISIBLE);
+				}
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int state) {
+
+			}
+		});
 
 		//设置配置器 ViewPager是ViewGroup
 		mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
 			@Override
 			public Fragment getItem(int position) {
-				return fragments.get(position);
+				return mFragments.get(position);
 			}
 
 			@Override
 			public int getCount() {
-				return fragments.size();
+				return mFragments.size();
 			}
 
 			@Override
 			public CharSequence getPageTitle(int position) {
-				return titles.get(position);
+				return mTitles.get(position);
 			}
 		});
 
 		//绑定
 		mTabLayout.setupWithViewPager(mViewPager);
 
-		//A去掉ActionBar的渐进阴影
+		//去掉ActionBar的渐进阴影
 		//noinspection ConstantConditions
 		getSupportActionBar().setElevation(0);
+	}
+
+	private void initData() {
+		mTitles = new StringGenerator("title").generate(mSize);
+		mWords = new StringGenerator("Fragment").generate(mSize);
+
+		mFragments = new ArrayList<>();
+		for (int i = 0; i < mSize; i++) {
+			TabBlankFragment fragment = new TabBlankFragment();
+			fragment.setTitle(mWords.get(i));
+			mFragments.add(fragment);
+		}
 	}
 }
