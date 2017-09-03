0f9fca3f0b80cb9403555c9d136818cf5e0c1161
Rongxin Sun <sunrongxin7666@163.com>
2017-09-03 17:40:29 +0800

    coordinatorlayout 与CollapsingToolbarLayout 自定义Behavior

--- app/src/main/java/com/android/srx/github/designsupportlibrarydemo/CoordinatorLayoutActivity.java
+++ app/src/main/java/com/android/srx/github/designsupportlibrarydemo/CoordinatorLayoutActivity.java
@@ -1,15 +1,33 @@
 package com.android.srx.github.designsupportlibrarydemo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
 
 import com.android.srx.github.designsupportlibrarydemo.R;
+import com.android.srx.github.designsupportlibrarydemo.databinding.ActivityCoordinatorLayoutBinding;
+import com.android.srx.github.designsupportlibrarydemo.utils.StringGenerator;
 
 public class CoordinatorLayoutActivity extends AppCompatActivity {
+	private ActivityCoordinatorLayoutBinding mBinding;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_coordinator_layout);
+		mBinding = DataBindingUtil.setContentView(this,R.layout.activity_coordinator_layout);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		initView();
 	}
+
+	private void initView() {
+		mBinding.collapsingToolbar.setTitle("Design");
+		mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		mBinding.recyclerView.setAdapter(
+				new ListAdapter(this, new StringGenerator("Item ").generateList(50)));
+	}
+
+
 }
